83ee0148837cbe01319a2164bdda3c85c10b9a36
Waldrey <waldreysouzasilva@outlook.com>
2019-06-25 21:21:37 -0300

    🔧 database config

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,16 @@
-var sql = require('mysql');
+const sql = require('mysql')
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env'
+})
 
-var connection = function() {
-  return sql.createConnection({
-    host: 'mysql669.umbler.com',
-    port: '41890',
-    user: 'devops_cobertura',
-    password: 'r00T7790',
-    database: 'cobertura'
-  });
-};
+console.log(process.env.DB_USER)
+var connection = () => (
+  sql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  })
+)
 
-module.exports = function() {
-  return connection();
-};
+module.exports = () => connection()
